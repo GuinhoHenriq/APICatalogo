7d11e0366aac12177dfa2c7438db2ad1b54ced36
Guilherme <Guilherme@Notebook-Vaio>
2021-03-15 12:38:39 -0300

    Migration

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -18,9 +18,11 @@ namespace APICatalogo.Models
         [Required]
         [StringLength(300)]
         public string Descricao { get; set; }
-        [Required]
         
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
         public decimal Preco { get; set; }
+
         [Required]
         [StringLength(300)]
         public string ImagemUrl { get; set; }
