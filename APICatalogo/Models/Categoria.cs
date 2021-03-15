f3d7f8f979c3fc5eeb857febab1c4a0ad1572700
Guilherme Henrique <guinho0010@hotmail.com>
2021-03-15 10:43:55 -0300

    Add project files.

---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Models
+{
+    [Table("Categorias")]
+    public class Categoria
+    {
+        // Quando se tem uma definiçao de coleçao deve-se 
+        // definir a inicializaçao desta coleçao.
+        // Como descrito abaixo
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+        [Key]
+        public int CategoriaId { get; set; }
+        [Required]
+        [MaxLength(80)]
+        public string Nome { get; set; }
+        [Required]
+        [MaxLength(300)]
+
+        public string ImagemUrl { get; set; }
+
+        // Propriedade de navegaçao - Relacionamento entre as tabelas - EF
+        // Uma categoria possui muitos produtos - 1-N
+        public ICollection<Produto> Produtos { get; set; }
+    }
+}
---
+++ APICatalogo/Models/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.Models
+{
+    [Table("Produtos")]
+    public class Produto
+    {
+        [Key]
+        public int produtoId { get; set; }
+        [Required]
+        [StringLength(80)]
+        public string Nome { get; set; }
+        [Required]
+        [StringLength(300)]
+        public string Descricao { get; set; }
+        [Required]
+        
+        public decimal Preco { get; set; }
+        [Required]
+        [StringLength(300)]
+        public string ImagemUrl { get; set; }
+
+        public float Estoque { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public Categoria Categoria { get; set; }
+
+        public int CategoriaID { get; set; }
+    }
+}
